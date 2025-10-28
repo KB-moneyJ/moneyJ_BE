6bed934992cc6d5385261230cb5f06cec68a6368
mojiri00 <moguricamera@gmail.com>
2025-10-28 19:54:58 +0900

    feat(AccountControllerApiSpec.java): Account 스웨거 추가

--- src/main/java/com/project/moneyj/account/controller/AccountController.java
+++ src/main/java/com/project/moneyj/account/controller/AccountController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/codef/accounts")
-public class AccountController {
+public class AccountController implements AccountControllerApiSpec{
 
     private final AccountService accountService;
 
