7f5fe63b2f250955ae2ce809359206b1bf2fecf0
mojiri00 <moguricamera@gmail.com>
2025-10-28 21:35:44 +0900

    refactor(AccountControllerApiSpec.java): 줄바꿈 적용

--- src/main/java/com/project/moneyj/account/controller/AccountControllerApiSpec.java
+++ src/main/java/com/project/moneyj/account/controller/AccountControllerApiSpec.java
@@ -11,17 +11,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-@Tag(name = "Accounts", description = "여행 플랜 계좌 API") // CodefAuthController와 통일감을 주기 위해
+@Tag(name = "Accounts", description = "여행 플랜 계좌 API")
 @RequestMapping("/api/codef/accounts")
 public interface AccountControllerApiSpec {
 
     @Operation(summary = "여행별 선택한 계좌를 DB에 저장", description = "사용자가 선택한 계좌를 DB에 저장합니다.")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "계좌 저장 성공"), // 201 CREATED
+            @ApiResponse(responseCode = "201", description = "계좌 저장 성공"),
             @ApiResponse(responseCode = "400", description = "잘못된 요청")
     })
     @PostMapping("/bank")
     ResponseEntity<AccountLinkResponseDTO> linkAccount(
             @AuthenticationPrincipal CustomOAuth2User customUser,
-            @RequestBody AccountLinkRequestDTO request);
+            @RequestBody AccountLinkRequestDTO request
+    );
 }
