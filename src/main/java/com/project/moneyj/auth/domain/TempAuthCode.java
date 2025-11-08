95ca88fdda1a8c4860549bee07a7f3dddb78fc4b
mojiri00 <moguricamera@gmail.com>
2025-11-08 14:53:00 +0900

    refactor(Transaction): 정적 팩토리 메서드 도입

--- src/main/java/com/project/moneyj/auth/domain/TempAuthCode.java
+++ src/main/java/com/project/moneyj/auth/domain/TempAuthCode.java
@@ -5,16 +5,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+
+import lombok.*;
 
 @Entity
 @Getter
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "temp_auth_code")
 public class TempAuthCode {
     @Id
