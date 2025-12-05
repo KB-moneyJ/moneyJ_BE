ae2b0778510fa3a042e1883565f8fd424a2a2dcc
mojiri00 <moguricamera@gmail.com>
2025-12-05 15:43:26 +0900

    feat(TripController): getBalances() userId 파라미터 추가

--- src/main/java/com/project/moneyj/trip/controller/TripController.java
+++ src/main/java/com/project/moneyj/trip/controller/TripController.java
@@ -103,8 +103,11 @@ public class TripController implements TripControllerApiSpec{
      * 마지막 동기화 >= 3시간: CODEF 비동기 호출
      */
     @GetMapping("/{tripPlanId}/balances")
-    public List<UserBalanceResponseDTO> getBalances(@PathVariable Long tripPlanId) {
-        return tripPlanService.getUserBalances(tripPlanId);
+    public ResponseEntity<UserBalanceResponseDTO> getBalances(@AuthenticationPrincipal CustomOAuth2User customUser,
+                                                              @PathVariable Long tripPlanId) {
+        Long userId = customUser.getUserId();
+        UserBalanceResponseDTO response = tripPlanService.getUserBalances(userId, tripPlanId);
+        return ResponseEntity.ok(response);
     }
 
     /**
