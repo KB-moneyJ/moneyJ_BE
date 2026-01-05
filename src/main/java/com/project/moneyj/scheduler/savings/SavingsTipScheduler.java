87e99889c439208440928500d99c635c47d2d309
Youngjae so <terranbang@gmail.com>
2026-01-05 16:30:50 +0900

    feat(savings): 저축 팁 스케쥴러 추가

---
+++ src/main/java/com/project/moneyj/scheduler/savings/SavingsTipScheduler.java
@@ -0,0 +1,25 @@
+package com.project.moneyj.scheduler.savings;
+
+import com.project.moneyj.trip.service.TripPlanService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class SavingsTipScheduler {
+    private final TripPlanService tripPlanService;
+
+    /**
+     * 매월 1일 00:00에 저축 팁 갱신
+     */
+    @Scheduled(cron = "0 0 0 1 * ?", zone = "Asia/Seoul")
+    public void updateSavingsTip(){
+        log.info("저축 팁 갱신 스케쥴러 시작");
+        tripPlanService.updateAllMemberSavingTip();
+        log.info("저축 팁 갱신 스케쥴러 종료");
+    }
+
+}
