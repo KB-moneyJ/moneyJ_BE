dc6e5f19fc524913d594ba951edd604324d68418
mojiri00 <moguricamera@gmail.com>
2026-01-20 21:07:38 +0900

    feat: 카드 응답 통합 DTO 구현

---
+++ src/main/java/com/project/moneyj/card/dto/CardResponseDTO.java
@@ -0,0 +1,23 @@
+package com.project.moneyj.card.dto;
+
+import com.project.moneyj.card.domain.Card;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class CardResponseDTO {
+    private Long cardId;
+    private String cardName;
+    private String cardNo;
+    private String organization;
+
+    public static CardResponseDTO from(Card card) {
+        return CardResponseDTO.builder()
+                .cardId(card.getCardId())
+                .cardName(card.getCardName())
+                .cardNo(card.getCardNo())
+                .organization(card.getOrganization())
+                .build();
+    }
+}
