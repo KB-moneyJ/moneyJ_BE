e6a5887a9d1c4553ee5afcd99f6d3aa6b1daabee
jmmo0722 <moguricamera@gmail.com>
2026-01-23 21:58:34 +0900

    feat: @NotBlank 에노테이션 추가

--- src/main/java/com/project/moneyj/card/dto/CardLinkRequestDTO.java
+++ src/main/java/com/project/moneyj/card/dto/CardLinkRequestDTO.java
@@ -1,5 +1,6 @@
 package com.project.moneyj.card.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Getter
@@ -7,7 +8,10 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class CardLinkRequestDTO {
+
     private String cardName;
+    @NotBlank(message = "카드번호는 필수 입력입니다.")
     private String cardNo;
+    @NotBlank(message = "기관 코드는 필수 입력입니다.")
     private String organizationCode;
 }
