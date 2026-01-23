3d833c8e400131ba0f9bfdd5ee9203b2d84d53c4
jmmo0722 <moguricamera@gmail.com>
2026-01-23 15:49:24 +0900

    feat: @NotNull, @NotBlank 에노테이션 추가

--- src/main/java/com/project/moneyj/account/dto/AccountSwitchRequestDTO.java
+++ src/main/java/com/project/moneyj/account/dto/AccountSwitchRequestDTO.java
@@ -1,6 +1,7 @@
 package com.project.moneyj.account.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,4 +15,12 @@ public class AccountSwitchRequestDTO {
     @NotBlank(message = "계좌번호는 필수 입력입니다.")
     @Pattern(regexp = "^[0-9]*$", message = "숫자만 입력하세요!")
     private String accountNumber;
+
+    @NotNull(message = "잔액은 필수 입력입니다.")
+    private Integer balance;
+
+    @NotBlank(message = "기관 코드는 필수 입력입니다.")
+    private String organizationCode;
+
+    private String accountName;
 }
