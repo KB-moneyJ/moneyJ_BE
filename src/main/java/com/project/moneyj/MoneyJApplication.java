0f14215d2605948b79c192ebb3f822f5b675bf1b
mojiri00 <moguricamera@gmail.com>
2026-02-17 18:37:25 +0900

    refactor: @EnableAsync 삭제

--- src/main/java/com/project/moneyj/MoneyJApplication.java
+++ src/main/java/com/project/moneyj/MoneyJApplication.java
@@ -9,7 +9,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 @EnableJpaAuditing
-@EnableAsync
 public class MoneyJApplication {
 
 	public static void main(String[] args) {
